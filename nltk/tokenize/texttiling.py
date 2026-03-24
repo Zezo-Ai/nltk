ad9c96ba00a16923ffe917eacf63f1707bfa2d08
Steven Bird <stevenbird1@gmail.com>
2026-03-24 15:39:00 +0930

    Update copyright year

--- nltk/tokenize/texttiling.py
+++ nltk/tokenize/texttiling.py
@@ -1,6 +1,6 @@
 # Natural Language Toolkit: TextTiling
 #
-# Copyright (C) 2001-2025 NLTK Project
+# Copyright (C) 2001-2026 NLTK Project
 # Author: George Boutsioukis
 #
 # URL: <https://www.nltk.org/>
